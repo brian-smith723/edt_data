0a3be88f31eb55a0f38e5ba16e4291e6ae67f9dc
Brian Smith <n00u9204@gmail.com>
2017-01-22 16:11:23 -0500

    fred refactor

--- __init__.py
+++
@@ -1,6 +0,0 @@
-__version__ = version = '0.0.1'
-
-#from .data import (DataWriter) get_components_yahoo, get_data_famafrench, get_data_google, get_data_yahoo,  # noqa
-        #get_data_yahoo_actions, get_quote_google, get_quote_yahoo, DataReader, Options)  # noqa
-
-
